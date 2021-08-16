16cc4d462bfc6e74d8b961fa35dbe232796cbd28
Shravan Chintha <schintha@opentext.com>
2021-08-16 19:12:00 +0530

    pomtraining

---
+++ src/test/java/tests/ChromeDriverTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ChromeDriverTest {
+
+	@Test
+	public void testCase() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\SeleniumSoftware\\browserexecutables\\chrome\\chromedriver_win32\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"))
+				.sendKeys("shravan");
+
+		WebElement element = driver
+				.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input"));
+		element.sendKeys("shravan");
+
+		driver.findElement(By.xpath("/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]")).click();
+		driver.close();
+		driver.quit();
+	}
+
+}
